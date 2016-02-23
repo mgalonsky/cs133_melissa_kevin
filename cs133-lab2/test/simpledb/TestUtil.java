a454382df9b9fc7b3674735ead09e7ec7fc34a0d
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2016-02-22 23:38:33 -0800

    Modified testutil to make it's debugging output more useful.

--- cs133-lab2/test/simpledb/TestUtil.java
+++ cs133-lab2/test/simpledb/TestUtil.java
@@ -141,6 +141,7 @@ public class TestUtil {
                     matched = true;
                     break;
                 }
+                System.out.print(next.toString());
             }
 
             if (!matched) {
