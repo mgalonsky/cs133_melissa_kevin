822a2dbad3f83d616e6480584a05a91f8f273982
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2016-02-22 22:51:26 -0800

    Wrote String Aggregator.

--- cs133-lab2/src/java/simpledb/StringAggregator.java
+++ cs133-lab2/src/java/simpledb/StringAggregator.java
@@ -1,5 +1,11 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import simpledb.Aggregator.Op;
+
 /**
  * Computes some aggregate over a set of StringFields.
  */
@@ -7,6 +13,10 @@ public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
 
+    
+    private int gbfield;
+    private Type gbfieldtype;
+    private int field;
     /**
      * Aggregate constructor
      * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
@@ -17,15 +27,39 @@ public class StringAggregator implements Aggregator {
      */
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+        if (what != Op.COUNT) {
+        	throw new IllegalArgumentException();
+        }
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        
+        groups = new HashMap<Field, Integer>();
     }
 
+    
+    private HashMap<Field, Integer> groups;
+    private int count;
     /**
      * Merge a new tuple into the aggregate, grouping as indicated in the constructor
      * @param tup the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+    	count = 0;
+    	
+    	if(gbfield != NO_GROUPING && 
+    			groups.containsKey(tup.getField(gbfield))){
+    		count =  groups.get(tup.getField(gbfield));
+    	} else if (groups.containsKey(null)) {
+    		count =  groups.get(null);
+    	}
+    	count++;
+    	
+    	if (gbfield != NO_GROUPING) {
+    		groups.put(tup.getField(gbfield), count);    
+    	} else {
+    		groups.put(null, count);    
+    	}
+    		
     }
 
     /**
@@ -37,8 +71,45 @@ public class StringAggregator implements Aggregator {
      *   aggregate specified in the constructor.
      */
     public DbIterator iterator() {
-        // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+ArrayList<Tuple> tuples = new ArrayList<Tuple>();
+    	
+    	if (gbfield == NO_GROUPING) {
+    		Type[] type = new Type[1];
+    		type[0] = Type.INT_TYPE;
+    		TupleDesc desc = new TupleDesc(type);
+    		Tuple tuple = new Tuple(desc);
+    		Integer vals = groups.get(null);
+    		tuple.setField(0,  new IntField(vals));
+    		tuples.add(tuple);
+    		TupleIterator iterToRet = new TupleIterator(desc, tuples);
+        	return iterToRet;
+    	}
+    	
+    	Type[] gbtype = new Type[2];
+    	gbtype[0] = gbfieldtype;
+    	gbtype[1] = Type.INT_TYPE;
+    	
+    	
+    	
+    	TupleDesc desc = new TupleDesc(gbtype);
+    	
+    	Iterator<Field> iter = this.groups.keySet().iterator();
+    	
+    	int i = 0;
+    	while(iter.hasNext()) {
+    		Field nextField = iter.next();
+    		Tuple tup = new Tuple(desc);
+    		
+    		tup.setField(0, nextField);
+    		Integer vals = groups.get(nextField);
+    		
+    		tup.setField(1, new IntField(vals));
+    		tuples.add(tup);
+    		
+    	}
+    	
+    	TupleIterator iterToRet = new TupleIterator(desc, tuples);
+    	return iterToRet;
     }
 
 }
